559e8def7bcc4324d7050e1f2c305826a698d28d
Office Account <officeaccount@nomans-MacBook-Pro.local>
2017-03-08 15:48:39 +0500

    comited

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -26,7 +26,7 @@ class UsersController extends ParentController
     public function updateProfilePicture(UpdateProfilePictureRequest $request)
     {
         $user = clone($request->user);
-        $user = $this->usersRepo->store($user->setProfilePicture($this->saveProfilePicture($request->file('profile_picture'))));
+        $user = $this->usersRepo->update($user->setProfilePicture($this->saveProfilePicture($request->file('profile_picture')))->setImageSetted(1));
         $user = $user->setProfilePicture(env('APP_URL').$user->getProfilePicture());
         return $this->response->respond(['data'=>[
             'user'=>$user->toJson()
